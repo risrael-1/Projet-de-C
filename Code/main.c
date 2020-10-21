e535ffa61c2c8deeb634e180deaa91c5c44ff13c
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2020-10-21 21:42:53 +0200

    Add the fuction to get the size of file [WIP] parseToArray function

--- Code/main.c
+++ Code/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 FILE *openFile(char *fileName) {
     FILE *f = fopen(fileName, "r");
@@ -14,10 +15,51 @@ FILE *openFile(char *fileName) {
 
 }
 
+long countLine(FILE* file){
+    fseek(file, 0 , SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0 , SEEK_SET);// needed for next read from beginning of file
+
+    return fileSize;
+}
+
+char** parseToArray(FILE* file, int* top){
+
+    long count = countLine(file);
+
+    fseek(file,0,SEEK_SET);
+
+    char** document = malloc(sizeof(char)*count);
+
+    char* line = malloc(sizeof(char)*100);
+    while ( fgets( line, 100, file ) != NULL )
+    {
+        document[(*top)] =  malloc(sizeof(char)*100);
+        strcpy(document[(*top)], line);
+
+        *top +=1;
+    }
+
+    return document;
+}
+
 int main() {
-    openFile("../exemple.xml");
+    FILE* file1 = openFile("../exemple.xml");
     openFile("../test.dtd");
+
+    int top = 0;
+    char** document = parseToArray(file1,&top);
+
+    for (int i = 0; i < top; i++) {
+        printf("%s", document[i]);
+    }
+
+
     printf("Hello, World!\n");
+
+
+
+
     return 0;
 }
 
