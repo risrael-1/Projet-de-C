0b99837c744f8abcbe56c7b3423f4686ea2ea3f5
risrael-1 <rapha.israel@live.fr>
2020-10-21 20:03:13 +0200

    new func for read file

--- Code/main.c
+++ Code/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+FILE *openFile(char *fileName) {
+    FILE *f = fopen(fileName, "r");
+    if(f != NULL) {
+        //printf("ok");
+        return f;
+    } else {
+        //printf("echec");
+        return NULL;
+       //
+    }
+
+}
 
 int main() {
+    openFile("../exemple.xml");
+    openFile("../test.dtd");
     printf("Hello, World!\n");
     return 0;
 }
+
+
+
