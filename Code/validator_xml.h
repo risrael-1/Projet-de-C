ff3e9ce12b1193caa2187eefbb21c6a9b4db5678
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2020-10-22 05:10:54 +0200

    We can get the XMLtag BEWARE of the simple tag like : <simpletag> 	value </simpletag>
    I think we will rewrite the function getXMLTag because of this case :(
    (issue : #1)

--- Code/validator_xml.h
+++ Code/validator_xml.h
@@ -1,7 +1,22 @@
 
+typedef struct XMLTag{
+
+    char* name;
+    char* value;
+    int isSimpleElement;
+    int isEndOfSet;
+
+}XMLTag;
 
 FILE *openFile(char *fileName);
 
 long countLine(FILE* file);
 
 char** parseToArray(FILE* file, int* top);
+
+char* substring(char* haystack, int start, int length);
+
+int strpos(char* haystack, char* value);
+
+XMLTag getXMLTag(char* line);
+
